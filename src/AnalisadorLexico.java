99b1fc8552baa5d9e10a66b1932b74013068e45d
Camille Jesus <camillejesus27@gmail.com>
2019-04-09 12:34:46 -0300

    Gerando alguns tokens.

--- src/AnalisadorLexico.java
+++ src/AnalisadorLexico.java
@@ -1,30 +1,102 @@
-import token.PalavrasReservadas;
-import token.Token;
+import token.*;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class AnalisadorLexico {
 
+    private static ArrayList<String> auxLexemas = new ArrayList<>();
+    private static ArrayList<Integer> auxLinhas = new ArrayList<>();
+    private static ArrayList<Token> tokens = new ArrayList<>();
+    private static int totalLinhas = 0;
+
     public static void main(String[] args) {
-        int linha = 0;
-        String lexema, classe, erro;
-        Scanner entrada = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("Digite o lexema:");
-            lexema = entrada.nextLine();
-            classe = classificaLexema(lexema);
+        try {
+            lerArquivo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        identificaLexema();
+    }
 
-            if (!classe.equals("CLASSE_INVALIDA")) {
-                Token token = new Token(classe, lexema, linha);
-                linha++;
-                System.out.println(token.toString()+"\n");
-            } else {
-                erro = classificaErro(lexema);
-                System.out.println("Erro: " + erro + "\n");
+    public static void lerArquivo() throws IOException {
+        FileReader arquivo = new FileReader("teste/entrada1.txt");
+        BufferedReader buffer = new BufferedReader(arquivo);
+        String linha;
+        boolean comentarioBloco = false;
+
+
+        //Lê todas as linhas do arquivo até o final:
+        while ((linha = buffer.readLine()) != null) {
+            totalLinhas++;
+
+            if (linha.contains("/*")) {
+                linha = linha.replaceAll("/\\*.*", "");
+                comentarioBloco = true;
+                separaLinha(linha);
             }
 
+            if (comentarioBloco == false) {
+
+                if (linha.contains("//")) {
+                    linha = linha.replaceAll("//.*", "");
+                }
+            } else {
+
+                if (linha.contains("*/")) {
+                    linha = linha.replaceAll(".*\\*/", "");
+                    comentarioBloco = false;
+                } else {
+                    linha = linha.replaceAll(".*", "");
+                }
+            }
+            separaLinha(linha);
+        }
+        arquivo.close();
+        //System.out.println(totalLinhas);
+    }
+
+    public static void separaLinha(String linha) {
+        String partes[] = linha.split("\\s+");
+
+        for (int i = 0; i < partes.length; i++) {
+            //System.out.println(partes[i]);
+
+            if (!partes[i].isEmpty()) {
+                auxLexemas.add(partes[i]);
+                auxLinhas.add(totalLinhas);
+            }
+        }
+    }
+
+    public static void identificaLexema() {
+        StringBuilder lexema = new StringBuilder();
+        String classe = "", auxLexema, auxLexema2 = "";
+
+        for (int i = 0; i < auxLexemas.size(); i++) {
+            auxLexema = auxLexemas.get(i);
+            System.out.println(auxLexemas.get(i));
+
+            if (!(classe = classificaLexema(auxLexema)).equals("CLASSE_INVALIDA")) {
+                tokens.add(new Token(classe, auxLexema, auxLinhas.get(i)));
+            } else {
+
+                for (int j = 0; j < auxLexemas.get(i).length(); j++) {
+                    /*System.out.println(auxLexemas.get(i).charAt(j)); a=2
+                    //lexema.append(auxLexema.charAt(j));
+                    //VOU IMPLEMENTAR AQUI PARA PERCORRER CARACTERE POR CARACTERE
+
+                    if (!(classe = classificaLexema(auxLexema.toString())).equals("CLASSE_INVALIDA")) {
+                        tokens.add(new Token(classe, auxLexema, auxLinhas.get(i)));
+                    }
+
+                    */
+                }
+            }
         }
     }
 
@@ -46,10 +118,6 @@ public class AnalisadorLexico {
             return "OPERADOR_LOGICO";
         } else if (lexema.matches(";|,|\\(|\\)|[|]|\\{|}|\\.")) {
             return "DELIMITADOR";
-        } else if (lexema.matches("//")) {   //Modificar para reconhecer até a quebra de linha.
-            return "COMENTARIO_LINHA";
-        } else if (lexema.matches("/\\*(.|\n)*\\*/")) {
-            return "COMENTARIO_BLOCO";
         } else if (lexema.matches("\"((\\\\\")|[^\"]|\\n)*\"")) {
             return "CADEIA_CARACTERES";
         }
